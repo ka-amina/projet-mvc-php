7cd93ad82ebb20721ab9f56ca197c8a6f6463b32
ka-amina <aminakara400@gmail.com>
2025-02-05 19:01:38 +0100

    add 404 page

--- core/Router.php
+++ core/Router.php
@@ -29,6 +29,7 @@ class Router extends controller
 
     public function dispatch()
     {
+
         $uri = strtok($_SERVER['REQUEST_URI'], '?');
         $method =  $_SERVER['REQUEST_METHOD'];
         if (array_key_exists($uri, $this->routes[$method])) {
@@ -37,6 +38,9 @@ class Router extends controller
 
             $controller = new $controller();
             $controller->$action();
-        } 
+        } else {
+            $this->render('404');
+            
+        }
     }
 }
